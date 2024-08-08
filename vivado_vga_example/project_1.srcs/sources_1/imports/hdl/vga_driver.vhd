cc39fde414e7a620f417789082aba476606bf659
femto-code <59444046+femto-code@users.noreply.github.com>
2024-08-08 20:33:49 +0200

    vga_driver: interface with wishbone bus

--- vivado_vga_example/project_1.srcs/sources_1/imports/hdl/vga_driver.vhd
+++ vivado_vga_example/project_1.srcs/sources_1/imports/hdl/vga_driver.vhd
@@ -1,31 +1,61 @@
-entity  is
+entity is
     Port ( 
-            -- wishbone bus
-            WB_CLK_I : in  STD_LOGIC;   --data clock
-            WB_RST_I : in  STD_LOGIC;   --reset
-            WB_DAT_I : in  STD_LOGIC_VECTOR (31 downto 0); --data
-            WB_DAT_O : out  STD_LOGIC_VECTOR (31 downto 0); --data
-    
-           VGA_CLK_I : in  STD_LOGIC;   --25Mhz
+        -- wishbone bus
+        CLK_I   : in  std_logic;
+        RST_I   : in  std_logic;
+        DAT_I   : in  std_logic_vector(7 downto 0);
+        STB_I   : in  std_logic;
+        ACK_O   : out std_logic;
 
-           -- output to physical VGA connector
-           VGA_HS_O : out  STD_LOGIC;
-           VGA_VS_O : out  STD_LOGIC;
-           VGA_R : out  STD_LOGIC_VECTOR (3 downto 0); -- Todo: change to 1 downto 0
-           VGA_B : out  STD_LOGIC_VECTOR (3 downto 0); -- Todo: change to 1 downto 0
-           VGA_G : out  STD_LOGIC_VECTOR (3 downto 0)); -- Todo: change to 1 downto 0
+        VGA_CLK_I : in  STD_LOGIC;   -- 25Mhz
+
+        -- output to physical VGA connector
+        VGA_HS_O : out  STD_LOGIC;
+        VGA_VS_O : out  STD_LOGIC;
+        VGA_R : out  STD_LOGIC_VECTOR (1 downto 0);
+        VGA_B : out  STD_LOGIC_VECTOR (1 downto 0);
+        VGA_G : out  STD_LOGIC_VECTOR (1 downto 0));
 end top;
 
-
-
 architecture Behavioral of top is
-
+    signal received_data : std_logic_vector(7 downto 0);
+    signal data_valid : std_logic := '0';
+    signal vga_sync : std_logic;
 begin
 
--- process
-´-- 2bit red, 2bit green, 2bit blue
--- Todo, please convert data from wishbone to RGB
+    -- decoding wishbone payload
+    process(CLK_I)
+    begin
+        if rising_edge(CLK_I) then
+            vga_sync <= '1' when received_data(1 downto 0) = "11" else '0';
+            if RST_I = '1' then
+                ACK_O <= '0';
+                received_data <= (others => '0');
+                data_valid <= '0';
+            else
+                if STB_I = '1' then
+                    -- Receive data
+                    received_data <= DAT_I;
+                    data_valid <= '1';
+                    ACK_O <= '1';
+                else
+                    ACK_O <= '0';
+                    data_valid <= '0';
+                end if;
+            end if;
+        end if;
+    end process;
 
--- orginal vga scan logic
+    -- Process to use the received data
+    process(CLK_I)
+    begin
+        if rising_edge(CLK_I) then
+            if data_valid = '1' then
+                vga_red <= received_data(7 downto 6);
+                vga_green <= received_data(5 downto 4);
+                vga_blue <= received_data(3 downto 2);
+            end if;
+        end if;
+    end process;
 
 end Behavioral;
