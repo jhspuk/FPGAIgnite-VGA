d4ed0da984571b122a4e3b5f003692c7de4e295a
jhspuk <j.hugh.sp@gmail.com>
2024-08-09 16:00:44 +0200

    fixed minor bugs

--- tests/PPU_2/ppu.v
+++ tests/PPU_2/ppu.v
@@ -23,7 +23,7 @@ parameter buffer_size = buffer_num * 8 - 1;
 reg [buffer_index_bits:0] h_count; 
 reg [buffer_index_bits:0] v_count;
 
-reg [buffer_index_bits_both:0] counter;
+reg [buffer_index_bits:0] counter;
 
 reg [(buffer_num * 8) - 1:0] ring_line;
 reg [buffer_index_bits:0] ring_index;
@@ -37,21 +37,11 @@ always @(posedge clk or posedge rst) begin
 	if (rst) begin
 		ring_line[255:0] <= {256{1'b0}};
 		ring_index <= 0;
-		h_count <= 0;
-		v_count <= 0;
-		stb_o <= 0;
-		data_o <= 0;
 		ack_i <= 0;
 	end else if(stb_i == 1) begin
 		ring_index <= ring_index + 1;
 		ring_line <= {ring_line[buffer_size - 8 : 0], data_i};
 		ack_i <= 1;
-		//if (ring_index == 0) begin
-		//	ring_line[buffer_size : buffer_size - 8] <= data_i;
-		//end else begin
-		//	ring_line[(ring_index + 1) * 8 : ring_index * 8 ] <= data_i;
-		//end
-		//	ack_i <= 1;
 	end else begin
 		ack_i <= 1'b0;
 	end	
@@ -64,7 +54,15 @@ end
 
 //output handshake
 always @(posedge clk) begin
-	if (ack_o) begin
+
+	if (rst) begin
+		data_o <= 0;
+		stb_o <= 0;
+		h_count <= 0;
+		v_count <= 0;
+		counter <= 0;
+		output_available <= 1;
+	end else if (ack_o) begin
 	
 		stb_o <= 0;
 		output_available <= 1'b1;
@@ -75,6 +73,7 @@ always @(posedge clk) begin
 		if (v_count == buffer_num - 1) begin
 			if (h_count == buffer_num - 1) begin
 				h_count <= 0;
+				counter <= counter + 1;
 			end else begin
 				h_count <= h_count + 1;
 			end
@@ -89,7 +88,49 @@ always @(posedge clk) begin
 		end
 		
 		//create output data
-		data_o <= data_i;
+		case (mode)
+			3'd0: begin
+				data_o <= data_i;
+			end
+			3'd1: begin
+				data_o [7:6] <= (v_count ^ h_count) > 0 ? ring_line[7:6] : 0;
+				data_o [5:4] <= (v_count ^ h_count) > 0 ? ring_line[5:4] : 0;
+				data_o [3:2] <= (v_count ^ h_count) > 0 ? ring_line[3:2] : 0;
+			end
+			3'd2: begin
+				data_o [7] <= (v_count ^ h_count + 1) < 1;
+				data_o [6] <= (v_count ^ h_count + 1) < 2;
+				data_o [5] <= (v_count ^ h_count) < 1;
+				data_o [4] <= (v_count ^ h_count) < 1;
+				data_o [3] <= (v_count ^ h_count - 1) < 1;
+				data_o [2] <= (v_count ^ h_count - 1) < 1;
+			end
+			3'd3: begin
+				data_o [7] <= (v_count ^ h_count) % 7 < 1; 
+				data_o [6] <= (v_count ^ h_count) % 7 < 1; 
+				data_o [5] <= (v_count ^ h_count) % 7 < 1;
+				data_o [4] <= (v_count ^ h_count) % 7 < 1;
+				data_o [3] <= (v_count ^ h_count) % 7 < 1;
+				data_o [2] <= (v_count ^ h_count) % 7 < 1;
+			end
+			3'd4: begin
+				data_o [7] <= ((v_count + counter) ^ h_count) % 7 < 1; 
+				data_o [6] <= ((v_count + counter) ^ h_count) % 7 < 1; 
+				data_o [5] <= ((v_count + counter + counter) ^ h_count) % 7 < 1;
+				data_o [4] <= ((v_count + counter + counter) ^ h_count) % 7 < 1;
+				data_o [3] <= (v_count ^ h_count) % 7 < 1;
+				data_o [2] <= (v_count ^ h_count) % 7 < 1;
+
+			end	
+			3'd5: begin
+			end
+			3'd6: begin
+			end
+			3'd7: begin
+			end
+			
+
+		endcase
 		
 		//strobe to confirm processing
 		stb_o <= 1;
